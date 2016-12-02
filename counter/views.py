68f4fcbf60cadee385e7fc386f0ccaa0e8a837d2
Jorge Escobar <jorge@fromzero.io>
2016-12-02 08:59:01 -0500

    First commit

---
+++ counter/views.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+
+from counter.models import Counter
+
+counter_app = Blueprint('counter_app', __name__)
+
+@counter_app.route('/')
+def init():
+    import pdb; pdb.set_trace()
+    counter = Counter.objects.all().first()
+    if counter:
+        counter.count += 1
+        counter.save()
+    else:
+        counter = Counter()
+        counter.count = 1
+        counter.save()
+    return "<h1>Counter: " + str(counter.count) + "</h1>"
---
+++ tests.py
@@ -0,0 +1,9 @@
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import unittest
+
+from counter.tests import CounterTest
+
+if __name__ == '__main__':
+    unittest.main()
